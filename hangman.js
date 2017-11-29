be888e0cb284cc72b30a3a16a602ee071b5d4b31
Martinhercka <32728864+Martinhercka@users.noreply.github.com>
2017-11-29 21:17:39 +0100

    added function generate random word&choose cat.
    added function generate random word&choose cat.

--- hangman.js
+++ hangman.js
@@ -1,1 +1,18 @@
-/*hangman.js*/
+var random=0;
+var word="";
+var animals=["macka","pes","prasa"];
+var capicity=["slavo","daj","tu","co","chces"];
+
+
+
+ 
+function animal(){
+    random = Math.floor(Math.random()*animals.length);
+    word = animals[random];
+    }
+
+ function capcity(){
+    random = Math.floor(Math.random()*capicity.length);
+    word = capicity[random];
+    console.log(random);
+    }
