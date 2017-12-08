ad25433c22a601afb89c3e20c329b28a54772330
lubomirvolcko <lubomir.volcko@akademiasovy.sk>
2017-12-08 02:08:12 +0100

    added comments

--- hangman.js
+++ hangman.js
@@ -1,3 +1,4 @@
+/* ##### START OF VARS #### */
 var random = 0;                                               //defined for generating word
 var word = "";                                                //defined for choesen word from random
 var easyAnimals = ["CAT", "MOUSE", "HORSE", "DOG", "LION"];   //defined field of animals
@@ -27,36 +28,39 @@ var lives=10;                                        		//define for lives
 var score=0;												//define for score
 var checkWord=0;											//define for counting right guessed word
 var checkScore;												//define for finish score
+/* ##### END OF VARS #### */
+
 function funNewgame(){ //set visible/unvisible div in html
-    document.getElementById('newgame').style.display = "none";
-    document.getElementById('difficulity').style.display = "block";
-    document.getElementById('alphabet').style.display = "none";
-	document.getElementById('canvas').style.display = "none";	document.getElementById('logo').style.display = "visible";
+    document.getElementById('newgame').style.display = "none"; //BUTTON newgame
+    document.getElementById('difficulity').style.display = "block"; //DIV difficility
+    document.getElementById('alphabet').style.display = "none"; //DIV alphabet
+	document.getElementById('divMistake').style.display = "none"; //DIV mistakes
+	document.getElementById('logo').style.display = "visible"; //IMG logo
     checkNewgame++; //for finding if was pressed new game
 } 
 
 function funDifficulity(value){ //set visible/unvisible div in html
-    document.getElementById('play').style.display = "none";
-    document.getElementById('difficulity').style.display = "none";
-    document.getElementById('category').style.display = "block"; 
-    document.getElementById('alphabet').style.display = "none";
-	document.getElementById('canvas').style.display = "none";
-	document.getElementById('logo').style.display = "visible";
-	document.getElementById('mainimg').style.display = "visible";
+    document.getElementById('play').style.display = "none"; //BUTTON play
+    document.getElementById('difficulity').style.display = "none"; //DIV difficility
+    document.getElementById('category').style.display = "block"; //DIV category
+    document.getElementById('alphabet').style.display = "none"; //DIV alphabet
+	document.getElementById('divMistake').style.display = "none"; //DIV mistakes
+	document.getElementById('logo').style.display = "visible"; //IMG logo
+	document.getElementById('mainimg').style.display = "visible"; //DIV mainimg
     gameDiff = value; //saveing choosen difficulity to var gameDiff
-    console.log("Difficulity: "+gameDiff+" (0-easy; 1-hard;)");
+    console.log("Difficulity: "+gameDiff+" (0-easy; 1-hard;)"); //print to console chosen difficulity
 }
 
 function funCategory(value){ //set visible/unvisible div in html
-    document.getElementById('play').style.display = "block";
-    document.getElementById('difficulity').style.display = "none";
-    document.getElementById('category').style.display = "none"; 
-    document.getElementById('alphabet').style.display = "none";
-	document.getElementById('canvas').style.display = "none";
-	document.getElementById('logo').style.display = "visible";
-	document.getElementById('mainimg').style.display = "visible";
+    document.getElementById('play').style.display = "block"; //BUTTON play
+    document.getElementById('difficulity').style.display = "none"; //DIV difficility
+    document.getElementById('category').style.display = "none"; //DIV category 
+    document.getElementById('alphabet').style.display = "none"; //DIV alphabet
+	document.getElementById('divMistake').style.display = "none"; //DIV mistakes
+	document.getElementById('logo').style.display = "visible"; //IMG logo
+	document.getElementById('mainimg').style.display = "visible"; //DIV mainimg
     gameCat = value; //saveing choosen category to var gameCat
-    console.log("Category: "+gameCat+" (0-animals; 1-cities;)");
+    console.log("Category: "+gameCat+" (0-animals; 1-cities;)"); //print to console chosen category
 }
 
 function checkLetter(value) { //function for chcecking pressed letter
@@ -64,97 +68,94 @@ function checkLetter(value) { //function for chcecking pressed letter
 	
 	var letter = alphabet[value]; //save pressed letter to var letter
 	
-        for(var i=0;i<word.length;i++)    //browse word length
+        for(var i=0;i<word.length;i++) //browse word length
         {
-            if(word[i]==letter && (lives>0 && checkScore!=score) && checkWord!=word.length)          //compare insert letter from user with guessed word
+            if(word[i]==letter && (lives>0 && checkScore!=score) && checkWord!=word.length) //compare insert letter from user with guessed word
             {
                 answerArray[i]=letter;	//if guessed letter is correct insert it to position
-				//document.getElementById('gWord').setAttribute("style", "color: #009fbc");
 				checkWord=checkWord+1;	//if guessed letter is correct add 1 to checkWord
-				score+=10;
-				if((lives>0 && checkScore!=score) || checkWord!=word.length)
+				score+=10; //add 10 points to score
+				if((lives>0 && checkScore!=score) || checkWord!=word.length) //compare lives if are greater than 0 and checkScore isn't like score
 					{	
-						document.getElementById('score').innerHTML = "Score: "+score;
+						document.getElementById('score').innerHTML = "Score: "+score; //print score 
 					}				
             }
 			
         }	
         var j = (word.indexOf(letter));
-      if (j == -1 && (lives>0 && checkScore!=score) && checkWord!=word.length) {
-		  lives -= 1;
-		  console.log("lives: "+lives);
-		  switch(lives)
+      if (j == -1 && lives>0 && checkScore!=score && checkWord!=word.length) 
+	  {
+		  lives -= 1; //sub lives of 1
+		  var imgMistake = document.getElementById('imgMistake');
+		  switch(lives) //printing img when is mistake
 		  {
-			  	case 9: document.getElementById('imgMistake').src="img/mistakes/mistake1.png";break;
-				case 8: document.getElementById('imgMistake').src="img/mistakes/mistake2.png";break;
-				case 7: document.getElementById('imgMistake').src="img/mistakes/mistake3.png";break;
-				case 6: document.getElementById('imgMistake').src="img/mistakes/mistake4.png";break;
-				case 5: document.getElementById('imgMistake').src="img/mistakes/mistake5.png";break;
-				case 4: document.getElementById('imgMistake').src="img/mistakes/mistake6.png";break;
-			  	case 3: document.getElementById('imgMistake').src="img/mistakes/mistake7.png";break;
-				case 2: document.getElementById('imgMistake').src="img/mistakes/mistake8.png";break;
-				case 1: document.getElementById('imgMistake').src="img/mistakes/mistake9.png";break;
-				case 0: document.getElementById('imgMistake').src="img/mistakes/mistake10.png";break;
+			  	case 9: imgMistake.src="img/mistakes/mistake1.png";break; //when lives=9 set other img
+				case 8: imgMistake.src="img/mistakes/mistake2.png";break; //when lives=8 set other img
+				case 7: imgMistake.src="img/mistakes/mistake3.png";break; //when lives=7 set other img
+				case 6: imgMistake.src="img/mistakes/mistake4.png";break; //when lives=6 set other img
+				case 5: imgMistake.src="img/mistakes/mistake5.png";break; //when lives=5 set other img
+				case 4: imgMistake.src="img/mistakes/mistake6.png";break; //when lives=4 set other img
+			  	case 3: imgMistake.src="img/mistakes/mistake7.png";break; //when lives=3 set other img
+				case 2: imgMistake.src="img/mistakes/mistake8.png";break; //when lives=2 set other img
+				case 1: imgMistake.src="img/mistakes/mistake9.png";break; //when lives=1 set other img
+				case 0: imgMistake.src="img/mistakes/mistake10.png";break; //when lives=0 set other img
 		  }
-		  console.log("IMG: mistake"+i);
-		  if(score>=5 && (lives>0 && checkScore!=score) && checkWord!=word.length)
+		  if(score>=5 && checkScore!=score && checkWord!=word.length) // if lives are bigger than 5 & checkScore isn't score & checkWord isn't word.length
 			  {
-				 	score-=5; 
-				  	document.getElementById('score').innerHTML = "Score: "+score;
-			  }else if(score==0 && checkScore==score)
+				 	score-=5; //sub 5 points from score
+				  	document.getElementById('score').innerHTML = "Score: "+score; // print score
+			  }else if(score==0 && checkScore==score) // if score is 0 & checkScore is like score
 				  {
-					  document.getElementById('score').innerHTML = "Score: "+score;
+					  document.getElementById('score').innerHTML = "Score: "+score; // print score
 				  }
       }
-        document.getElementById("gWord").innerHTML=answerArray.join(" ");  // print letter instead underline
+        document.getElementById("gWord").innerHTML=answerArray.join(" "); // print letter instead underline
 
-        if(lives>0)     // if lives reach 0 then print Game over
+        if(lives>0) // if lives reach 0 then print Game over
             {
-				if(checkWord==word.length)
+				if(checkWord==word.length) //if value of checkWord is the same like lenght of word
 					{
-						document.getElementById("guesses").innerHTML="You win!";
-						checkScore=score;
+						document.getElementById("lives").innerHTML="You win!"; //print You win! if user guess the word
+						checkScore=score; //set value of score to checkScore
 					}else{
-						document.getElementById("guesses").innerHTML="Lives: "+lives;
+						document.getElementById("lives").innerHTML="Lives: "+lives; //print lives
 					}
                 
-            }else if(lives<=0)
+            }else if(lives<=0) //if lives are 0 or less
                 {
-                    document.getElementById("guesses").innerHTML="Game Over!";
-					checkScore=score;
+                    document.getElementById("lives").innerHTML="Game Over!"; //print Game Over!
+					checkScore=score; //set value of score to checkScore
                 }
 
 		for(var g=0;g<26;g++) 
 		{
 			if(letter==alphabet[g] && (lives>0 && checkScore!=score) && checkWord!=word.length)
     		document.getElementById(alphabet[g]).style.visibility = "hidden"; //hide letter after click
-		}
-	
-	  
+		}	  
 
-        }
+        } /* ### END OF FUNCTION checkLetter ###*/
 
 function game () { //function for game
         
-        document.getElementById('play').style.display = "none";  //set visible/unvisible div in html 
-        document.getElementById('difficulity').style.display = "none"; //set visible/unvisible div in html
-        document.getElementById('category').style.display = "none"; //set category btn to unvisible
-        if(checkNewgame==0) //set aplhabet keyboard to unvisible
+        document.getElementById('play').style.display = "none"; //BUTTON play
+        document.getElementById('difficulity').style.display = "none"; //DIV difficility
+        document.getElementById('category').style.display = "none"; //DIV category
+        if(checkNewgame==0) //if WASN'T pressed button newgame
            {
-            document.getElementById('alphabet').style.display = "none";
-			document.getElementById('canvas').style.display = "none";
-			document.getElementById("guesses").style.display = "none";
-			document.getElementById('score').style.display = "none";
-           }else if(checkNewgame>0) //set aplhabet keyboard to visible
+            document.getElementById('alphabet').style.display = "none"; //DIV alphabet 
+			document.getElementById('divMistake').style.display = "none"; //DIV mistakes
+			document.getElementById("lives").style.display = "none"; //DIV lives
+			document.getElementById('score').style.display = "none"; //DIV score
+           }else if(checkNewgame>0) //if WAS pressed button newgame
            {
-			document.getElementById('logo').style.display = "none";
-			document.getElementById('mainimg').style.display = "none";
-            document.getElementById('alphabet').style.display = "block";
-			document.getElementById('canvas').style.display = "block";
-			document.getElementById("guesses").style.display = "block";
-			   document.getElementById("guesses").innerHTML="Lives: "+lives;
-			document.getElementById('score').style.display = "block";
-				document.getElementById('score').innerHTML = "Score: "+score;
+			document.getElementById('logo').style.display = "none"; //IMG logo
+			document.getElementById('mainimg').style.display = "none"; //DIV mainimg
+            document.getElementById('alphabet').style.display = "block"; //DIV alphabet
+			document.getElementById('divMistake').style.display = "block"; //DIV mistakes
+			document.getElementById("lives").style.display = "block"; //DIV lives
+			document.getElementById("lives").innerHTML="Lives: "+lives; //print lives
+			document.getElementById('score').style.display = "block"; //DIV score
+				document.getElementById('score').innerHTML = "Score: "+score; //print score
             }
         if(gameDiff==0) //if was choosen difficulity easy
           {
@@ -174,16 +175,16 @@ function game () { //function for game
                     {
                         random = Math.floor(Math.random()*hardAnimals.length); //generate random  word from field hardAnimals 
                         word = hardAnimals[random]; //save random word from field hardAnimals to var word
-                        document.getElementById('guesses').innerHTML = "Lives: "+lives;
+                        document.getElementById('lives').innerHTML = "Lives: "+lives; //print lives
                     }else if(gameCat==1) //if was choosen category cities
                     {
                         random = Math.floor(Math.random()*hardCities.length);  //generate random  word from field hardCities  
                         word = hardCities[random]; //save random word from field easyCities to var word
-                        document.getElementById('guesses').innerHTML = "Lives: "+lives;
+                        document.getElementById('lives').innerHTML = "Lives: "+lives; //print lives
                     }
               }
         
-            console.log(word);
+            console.log(word); //get to console random generated word
             
             
            for(var i = 0; i<word.length;i++)  //browse word length
@@ -193,4 +194,5 @@ function game () { //function for game
            }
            var s=answerArray.join(" "); //insert between underlines space
            document.getElementById("gWord").innerHTML= s; //write on screen underlines 
-}
+	
+} /* ### END OF FUNCTION game ###*/
